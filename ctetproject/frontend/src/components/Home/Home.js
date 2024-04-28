17033bfde3bfe041382633beb193d5c8459e9e6b
Murph Shaw <u7311689@anu.edu.au>
2024-04-28 17:23:39 +1000

    Animation changes and dynamic fonts into main

--- ctetproject/frontend/src/components/Home/Home.js
+++ ctetproject/frontend/src/components/Home/Home.js
@@ -5,8 +5,7 @@ function Home() {
   return (
     <section id="home" className="home-section">
       <h1>Conference Travel Emissions Tool</h1>
-      <h2>Ensure your conference is as carbon efficient as possible</h2>
-      <p>A tool developed for conference hosts, by conference hosts.</p>
+      <h2>Ensure your conference is as carbon efficient as possible. Developed for conference hosts, by conference hosts.</h2>
     </section>
   );
 }
