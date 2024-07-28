217a0d15bb532234c61be0b04087250b85aebbfc
shawn <u6948196@anu.edu.au>
2024-07-28 20:05:49 +1000

    hello

--- ctetproject/ctetproject/settings.py
+++ ctetproject/ctetproject/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ctetproject.onrender.com"]
 
 
 # Application definition
